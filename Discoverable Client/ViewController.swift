12a0b8be1df5cb6502ee0d9015036b14f685533b
Ben Mechen <benm123@yahoo.com>
2020-07-19 16:23:16 +0100

    Initial Commit

---
+++ Discoverable Client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Discoverable Client
+//
+//  Created by Ben Mechen on 19/07/2020.
+//  Copyright © 2020 Ben Mechen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
